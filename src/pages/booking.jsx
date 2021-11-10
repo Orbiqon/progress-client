2cc1dfd066d55173021f84bc4298abdb994fc8be
khurram khalil <khurram.khalil@orbiqon.com>
2021-11-10 16:01:02 +0500

    change in iframe link

--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -6,7 +6,10 @@ function Booking({ onClick, history }) {
   return (
     <Container>
       <div>
-        <iframe src="https://secure.gethealthie.com/appointments/embed_appt?dietitian_id=678293&appt_type_ids=[134619]" style="width:100%; height: 100%; min-height:600px; border:0px"></iframe>
+        <iframe
+          src="https://secure.gethealthie.com/appointments/embed_appt?dietitian_id=678293&appt_type_ids=[134619]"
+          style="width:100%; height: 100%; min-height:600px; border:0px"
+        ></iframe>
         <div className="flex pb-20 justify-center mt-20">
           <div className="flex items-center mr-20">
             <BackButton value="Back" color="black" />
