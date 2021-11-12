bb2476259160f0ec39704ba84bd0a4da26664873
khurram khalil <khurram.khalil@orbiqon.com>
2021-11-12 12:44:30 +0500

    changes image in support

--- src/pages/support.jsx
+++ src/pages/support.jsx
@@ -218,7 +218,7 @@ function Support() {
                 </picture>
 
                 <img
-                  className="absolute left:45% sm:left-53% lg:bottom-80 md:bottom-70 sm:bottom-90 sm:w-80% lg:rounded rounded-md transform-x bottom-120 h-100% w-60%"
+                  className="absolute left-45% sm:left-53% lg:bottom-80 md:bottom-70 sm:bottom-90 sm:w-80% lg:rounded rounded-md transform-x bottom-120 h-100% w-60%"
                   src="images/PROGRESS/Support.jpg"
                 />
                 {/* <picture>
